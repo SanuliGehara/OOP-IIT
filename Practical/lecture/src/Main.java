b07cdc74c11ec50f94744286cae7032673e6de4b
SanuliGehara <geharasanuli5@gmail.com>
2024-10-02 21:54:45 +0530

    Removed incorrect folders from remote repository

--- Practical/lecture/src/Main.java
+++
@@ -1,8 +0,0 @@
-import lk.sanu.oop.Rectangle;
-
-public class Main {
-    public static void main(String[] args) {
-        Rectangle r1 = new Rectangle(20,5);
-        System.out.println(r1);
-    }
-}
